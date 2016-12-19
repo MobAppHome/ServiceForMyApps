c2bb3f910ebe708ebff2b49ca96f4303ef2007db
Sattar Hummatli <settarxan@gmail.com>
2016-12-19 19:29:04 +0400

    Creating repo

---
+++ public/mah_android_updater_dir/mah_android_updater_millionaire_ge.php
@@ -0,0 +1,9 @@
+{
+    "is_run_mode":"true",
+    "name":"Millionaire EN",
+    "uri_current":"com.mobapphome.millionaire.en",
+    "version_code_current":"1",
+    "version_code_min":"1",
+    "update_info":"",
+    "update_date":"01/08/2016"
+}
---
+++ public/mah_android_updater_dir/mah_android_updater_milyoncu_aze.php
@@ -0,0 +1,9 @@
+{
+    "is_run_mode":"true",
+    "name":"Milyonçu AZE",
+    "uri_current":"com.mobapphome.milyoncu",
+    "version_code_current":"26",
+    "version_code_min":"22",
+    "update_info":"Yeni versiyada suallarda olan səhvlər aradan qaldırılıb",
+    "update_date":"14/07/2016"
+}
